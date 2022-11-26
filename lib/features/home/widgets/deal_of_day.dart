b3fceca609d9a43c473378499e35a9fabf84b836
mkaric2003 <mirza.karic595@gmail.com>
2022-11-26 18:59:32 +0100

    Home Screen UI

---
+++ lib/features/home/widgets/deal_of_day.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class DealOfDay extends StatefulWidget {
+  const DealOfDay({Key? key}) : super(key: key);
+
+  @override
+  State<DealOfDay> createState() => _DealOfDayState();
+}
+
+class _DealOfDayState extends State<DealOfDay> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.only(left: 10, top: 15),
+          child: const Text(
+            'Deal of the day',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        Image.network(
+          'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80',
+          height: 235,
+          fit: BoxFit.fitHeight,
+        ),
+        Container(
+          padding: const EdgeInsets.only(left: 15),
+          alignment: Alignment.topLeft,
+          child: const Text(
+            '\$1000',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
+          child: const Text(
+            'MacBook',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.network(
+                'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80',
+                fit: BoxFit.fitWidth,
+                width: 100,
+                height: 100,
+              ),
+              Image.network(
+                'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80',
+                fit: BoxFit.fitWidth,
+                width: 100,
+                height: 100,
+              ),
+              Image.network(
+                'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80',
+                fit: BoxFit.fitWidth,
+                width: 100,
+                height: 100,
+              ),
+              Image.network(
+                'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1120&q=80',
+                fit: BoxFit.fitWidth,
+                width: 100,
+                height: 100,
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 15).copyWith(left: 15),
+          alignment: Alignment.topLeft,
+          child: Text(
+            'See all details',
+            style: TextStyle(color: Colors.cyan[800]),
+          ),
+        ),
+      ],
+    );
+  }
+}
